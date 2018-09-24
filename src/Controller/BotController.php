9dd4c73dd3b3edc9444aac301240075f58b1b472
corentin-gillet <cgillet@w3com.fr>
2018-09-24 10:17:43 +0200

    r

---
+++ src/Controller/BotController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use BotMan\BotMan\BotMan;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BotController extends Controller
+{
+    public function handle(BotMan $botMan)
+    {
+    }
+}
